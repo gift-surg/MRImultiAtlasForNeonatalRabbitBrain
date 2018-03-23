19e8bdd652326ca7f87588ff36089ccf26abc556
Sebastiano Ferraris <sebastiano.ferraris@gmail.com>
2018-03-23 14:11:04 +0000

    ENh inter-rater boxplot add pandas manipulation data strcuture refactored

--- benchmarking/test_retest_intra_rater/d4_get_boxplot_all_labels.py
+++ benchmarking/test_retest_intra_rater/d4_get_boxplot_all_labels.py
@@ -17,9 +17,15 @@ if __name__ == '__main__':
 
     x_labels = ['man1 vs man2', 'auto vs man1', 'auto vs man2']
 
-    df_measures_man1_man2_taxo = pa.read_pickle(ph.pfi_pickled_scoring_man1_man2_all)
-    df_measures_auto_man1_taxo = pa.read_pickle(ph.pfi_pickled_scoring_auto_man1_all)
-    df_measures_auto_man2_taxo = pa.read_pickle(ph.pfi_pickled_scoring_auto_man2_all)
+    df_measures_man1_man2_all = pa.read_pickle(ph.pfi_pickled_scoring_man1_man2_all)
+    df_measures_auto_man1_all = pa.read_pickle(ph.pfi_pickled_scoring_auto_man1_all)
+    df_measures_auto_man2_all = pa.read_pickle(ph.pfi_pickled_scoring_auto_man2_all)
+
+    # get data data with the positive 1 - dice
+
+    df_measures_man1_man2_all["dice_smaller_than_1"] = df_measures_man1_man2_all['dice_score'] < 1.0
+    df_measures_auto_man1_all["dice_smaller_than_1"] = df_measures_auto_man1_all['dice_score'] < 1.0
+    df_measures_auto_man2_all["dice_smaller_than_1"] = df_measures_auto_man2_all['dice_score'] < 1.0
 
     plt.rc('font', family='serif')
 
@@ -30,31 +36,33 @@ if __name__ == '__main__':
 
     # Dice score
     ax[0].set_title('1 - Dice Score')
-    data_ds = [[1 - m for m in df_measures_man1_man2_taxo['dice_score'] if m > 10e-6],
-               [1 - m for m in df_measures_auto_man1_taxo['dice_score'] if m > 10e-6],
-               [1 - m for m in df_measures_auto_man2_taxo['dice_score'] if m > 10e-6]]
+    data_ds = [[1 - df_measures_man1_man2_all['dice_score'][id] for id in df_measures_man1_man2_all.index if df_measures_man1_man2_all["dice_smaller_than_1"][id]],
+               [1 - df_measures_auto_man1_all['dice_score'][id] for id in df_measures_auto_man1_all.index if df_measures_auto_man1_all["dice_smaller_than_1"][id]],
+               [1 - df_measures_auto_man2_all['dice_score'][id] for id in df_measures_auto_man2_all.index if df_measures_auto_man2_all["dice_smaller_than_1"][id]]]
     ax[0].boxplot(data_ds)
     ax[0].set_ylabel('%', fontsize=8)
-    ax[0].set_ylim([-0.001, 0.0450])  # set manually for easy comparisons between figures.
+    ax[0].set_ylim([-0.001, 0.10])  # set manually for easy comparisons between figures.
     ax[0].set_xticks(range(1, 4))
     ax[0].set_xticklabels(x_labels, rotation=45, fontsize=8)
 
     # Covariance Distance
     ax[1].set_title('CD')
-    data_cd = [[m for m in df_measures_man1_man2_taxo['covariance_distance'] if m > 10e-6],
-               [m for m in df_measures_auto_man1_taxo['covariance_distance'] if m > 10e-6],
-               [m for m in df_measures_auto_man2_taxo['covariance_distance'] if m > 10e-6]]
+    data_cd = [[df_measures_man1_man2_all['covariance_distance'][id] for id in df_measures_man1_man2_all.index if df_measures_man1_man2_all["dice_smaller_than_1"][id]],
+               [df_measures_auto_man1_all['covariance_distance'][id] for id in df_measures_auto_man1_all.index if df_measures_auto_man1_all["dice_smaller_than_1"][id]],
+               [df_measures_auto_man2_all['covariance_distance'][id] for id in df_measures_auto_man2_all.index if df_measures_auto_man2_all["dice_smaller_than_1"][id]]]
+
     ax[1].boxplot(data_cd)
     ax[1].set_ylabel('mm', fontsize=8)
-    ax[1].set_ylim([-0.001, 0.12])  # set manually for easy comparisons between figures.
+    ax[1].set_ylim([-0.001, 0.275])  # set manually for easy comparisons between figures.
     ax[1].set_xticks(range(1,4))
     ax[1].set_xticklabels(x_labels, rotation=45, fontsize=8)
 
     # Hausdorff Distance
     ax[2].set_title('HD')
-    data_hd = [[m for m in df_measures_man1_man2_taxo['hausdorff_distance'] if m > 10e-6],
-               [m for m in df_measures_auto_man1_taxo['hausdorff_distance'] if m > 10e-6],
-               [m for m in df_measures_auto_man2_taxo['hausdorff_distance'] if m > 10e-6]]
+    data_hd = [[df_measures_man1_man2_all['hausdorff_distance'][id] for id in df_measures_man1_man2_all.index if df_measures_man1_man2_all["dice_smaller_than_1"][id]],
+               [df_measures_auto_man1_all['hausdorff_distance'][id] for id in df_measures_auto_man1_all.index if df_measures_auto_man1_all["dice_smaller_than_1"][id]],
+               [df_measures_auto_man2_all['hausdorff_distance'][id] for id in df_measures_auto_man2_all.index if df_measures_auto_man2_all["dice_smaller_than_1"][id]]]
+
     ax[2].boxplot(data_hd)
     ax[2].set_ylabel('mm', fontsize=8)
     ax[2].set_ylim([-0.001, 0.7])  # set manually for easy comparisons between figures.
@@ -63,9 +71,9 @@ if __name__ == '__main__':
 
     # Normalised symmetric contour distance
     ax[3].set_title('NSCD')
-    data_nscd = [[m for m in df_measures_man1_man2_taxo['normalised_symmetric_contour_distance'] if m > 10e-6],
-                 [m for m in df_measures_auto_man1_taxo['normalised_symmetric_contour_distance'] if m > 10e-6],
-                 [m for m in df_measures_auto_man2_taxo['normalised_symmetric_contour_distance'] if m > 10e-6]]
+    data_nscd = [[df_measures_man1_man2_all['normalised_symmetric_contour_distance'][id] for id in df_measures_man1_man2_all.index if df_measures_man1_man2_all["dice_smaller_than_1"][id]],
+                 [df_measures_auto_man1_all['normalised_symmetric_contour_distance'][id] for id in df_measures_auto_man1_all.index if df_measures_auto_man1_all["dice_smaller_than_1"][id]],
+                 [df_measures_auto_man2_all['normalised_symmetric_contour_distance'][id] for id in df_measures_auto_man2_all.index if df_measures_auto_man2_all["dice_smaller_than_1"][id]]]
     ax[3].boxplot(data_nscd)
     ax[3].set_ylabel('mm', fontsize=8)
     ax[3].set_ylim([-0.001, 0.04])  # set manually for easy comparisons between figures.
